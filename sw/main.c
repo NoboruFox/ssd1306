dae5491307c3ff3093516944aa179d54be5b9a2f
Fox <werewolf.luner@gmail.com>
2022-03-27 03:36:56 +0300

    [WIP]: ssd1306 started

--- sw/main.c
+++ sw/main.c
@@ -1,21 +1,47 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
 
 void clock_setup()
 {
     /* Enable GPIOA clock. */
     rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_I2C1);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_set_i2c_clock_hsi(I2C1);
 }
 
 
 void gpio_setup()
 {
+    /* put your i2c gpio setup here */
     gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+	gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
 }
 
 void i2c_setup()
 {
+        /*put you i2c setup stuff here 
+         * (see stm32f03 example in libopencm3-examples) */
+
+	i2c_reset(I2C1);
+	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
+	i2c_peripheral_disable(I2C1);
+	//configure ANFOFF DNF[3:0] in CR1
+	i2c_enable_analog_filter(I2C1);
+    //digital filter is disabled
+	i2c_set_digital_filter(I2C1, 0);
+	/* HSI is at 8Mhz */
+	i2c_set_speed(I2C1, i2c_speed_sm_100k, 8);
+    //below no need?
+	//configure No-Stretch CR1 (only relevant in slave mode)
+	i2c_enable_stretching(I2C1);
+	//addressing mode
+	i2c_set_7bit_addr_mode(I2C1);
+	i2c_peripheral_enable(I2C1);
 }
+
 
 int main()
 {
